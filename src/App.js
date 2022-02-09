cd9238e6f88074707da00a2520a1346a6a08c3d1
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-02-09 13:07:27 +0800

    feat: ✨ basic usage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import {useQuery} from 'react-query'
+import {ReactQueryDevtools} from 'react-query/devtools'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function usePokemon() {
+  return useQuery(
+    'pokemons',
+    async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      return axios
+        .get(`https://pokeapi.co/api/v2/pokemon`)
+        .then((res) => res.data.results)
+    },
+    {
+      cacheTime: 5000, // time before inactive data gets GCed
+      staleTime: 0, // time before possible refetch
+      refetchOnWindowFocus: true,
+    },
+  )
 }
 
-export default App;
+function Pokemon() {
+  const queryInfo = usePokemon()
+
+  return queryInfo.isLoading ? (
+    'Loading...'
+  ) : queryInfo.isError ? (
+    queryInfo.error.message
+  ) : (
+    <div>
+      {queryInfo.data.map((result) => (
+        <div key={result.name}>{result.name}</div>
+      ))}
+      <br />
+      {queryInfo.isFetching ? 'Updating...' : null}
+    </div>
+  )
+}
+function Berries() {
+  const queryInfo = useQuery(
+    'berries',
+    async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      return axios
+        .get(`https://pokeapi.co/api/v2/berry`)
+        .then((res) => res.data.results)
+    },
+    {
+      cacheTime: 5000, // time before inactive data gets GCed
+      staleTime: 0, // time before possible refetch
+      refetchOnWindowFocus: true,
+    },
+  )
+
+  return queryInfo.isLoading ? (
+    'Loading...'
+  ) : queryInfo.isError ? (
+    queryInfo.error.message
+  ) : (
+    <div>
+      {queryInfo.data.map((result) => (
+        <div key={result.name}>{result.name}</div>
+      ))}
+      <br />
+      {queryInfo.isFetching ? 'Updating...' : null}
+    </div>
+  )
+}
+
+function Count() {
+  const queryInfo = usePokemon()
+  return <h3>You are looking at {queryInfo.data?.length ?? 0} pokemons.</h3>
+}
+
+function App() {
+  const [show, toggle] = React.useReducer((s) => !s, true)
+  return (
+    <>
+      <button onClick={toggle}> {show ? 'Hide' : 'Show'}</button>
+      <br />
+      {show ? (
+        <>
+          <Count />
+          <Pokemon />
+          <Berries />
+        </>
+      ) : null}
+      <ReactQueryDevtools />
+    </>
+  )
+}
+export default App
